7de65df2ac13f9dd2d5926f0529dc2141929f0f5
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-04-10 15:53:40 -0700

    improve http/https request script

--- http-chrome.sh
+++ http-chrome.sh
@@ -11,9 +11,8 @@ GET / HTTP/1.1
 Host: $1
 Connection: keep-alive
 Cache-Control: max-age=0
-User-Agent: Mozilla/5.0 AppleWebKit/537.36 Chrome/52.0.2743.116 Safari/537.36
+User-Agent: Mozilla/5.0 AppleWebKit/537 Chrome/50 Safari/537
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-
 EOF
 
 # get rid of Accept-Encoding which contains compressed format gzip
--- https-chrome.sh
+++ https-chrome.sh
@@ -1,24 +1,67 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-    echo "Usage: $0 <hostname>"
-    exit 1
-fi
-
-request=$(cat <<EOF
-GET / HTTP/1.1
-Host: $1
-Connection: keep-alive
-Cache-Control: max-age=0
-User-Agent: Mozilla/5.0 AppleWebKit/537.36 Chrome/52.0.2743.116 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-
+help=$(cat <<EOF
+Usage: ${0##*/} [-hvk] [-p PATH] DOMAIN/IP
+Make http request as if you were chrome.
+    -h         show this help
+    -v         be verbose (show request sent)
+    -p PATH    specify path name, default /
+    -m METHOD  specify http method, default GET
+    -s         secure, use https
+    -k         maintain http connection
 EOF
 )
 
-# echo "$request"
-{ echo "$request"; cat /dev/stdin; } |
-     ncat --ssl "$1" 443
-# somehow ncat would terminate connection when stdin is closed
-# keep connection open by catting stdin
+
+verbose=0
+path=/
+method=GET
+secure=0
+keepalive=0
+
+while getopts hvp:m:sk opt; do
+    case $opt in
+        h) echo "$help"; exit 0;;
+        v) verbose=1;;
+        p) path=$OPTARG;;
+        m) method=$OPTARG;;
+        s) secure=1;;
+        k) keepalive=1;;
+        *) echo "$help" >&2; exit 1;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [ -z "$1" ]
+then
+    echo "$help" >&2
+    exit 1
+fi
+host="$1"
+
+# debug
+echo "$host" $verbose $keepalive
+
+request=$(cat <<EOF
+$method $path HTTP/1.1
+Host: $host
+Connection: keep-alive
+Cache-Control: max-age=0
+User-Agent: Mozilla/5.0 AppleWebKit/537 Chrome/50 Safari/537
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+EOF
+)
+
+[ "$verbose" = 1 ] && echo "$request" && echo
+
+{
+    echo "$request"
+    echo
+    [ "$keepalive" = 1 ] && cat /dev/stdin
+} | {
+    if [ $secure = 1 ]; then
+        ncat --ssl "$host" 443
+    else
+        ncat "$host" 80
+    fi
+}
